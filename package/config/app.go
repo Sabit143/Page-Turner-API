1e2840149027078b0a4e4df3c84d8606ab8b278b
Sabit143 <sksabitali123@gmail.com>
2025-03-03 21:38:49 +0530

    some changes being made

--- package/config/app.go
+++ package/config/app.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect(){
-	d, err := gorm.Open("mysql", "akhil:Axlesharma@12@/simplerest?charset=utf8&parseTime=True&loc=Local")
+	d, err := gorm.Open("mysql", "sabit:sksabitali@1212/simplerest?charset=utf8&parseTime=True&loc=Local")
 	if err != nil{
 		panic(err)
 	}
